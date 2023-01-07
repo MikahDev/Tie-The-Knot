00f611c023d5af379e4cff3835659224a0be1476
Michael Villeprat <michael@Michaels-MBP.modem>
2023-01-07 19:02:43 +1100

    add authentification system

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,17 +5,38 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->passwordHasher = $hasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
         // $manager->persist($product);
 
+        $plainPassword = 'thevilleprats';
+
+        $adminRoles[] = 'ROLE_ADMIN';
+
         $admin = new User();
         $admin->setEmail('thevilleprats@gmail.com');
-        $admin->setPassword('thevilleprats');
+        $admin->setUsername('mikah');
+        $admin->setRoles($adminRoles);
+
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $admin,
+            $plainPassword
+        );
+        
+        $admin->setPassword($hashedPassword);
+            
         $manager->persist($admin);
 
         $manager->flush();
